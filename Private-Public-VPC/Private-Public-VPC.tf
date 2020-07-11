3749e5278d563b0269b098d7f340c83f09ba7c24
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-07-11 19:42:41 +0530

    Added subnets!

--- Private-Public-VPC/Private-Public-VPC.tf
+++ Private-Public-VPC/Private-Public-VPC.tf
@@ -13,8 +13,30 @@ resource "aws_key_pair" "Key-Pair" {
 
 // Creating a VPC!
 resource "aws_vpc" "custom" {
-  cidr_block       = "57.95.0.0/16"
+  cidr_block = "57.95.0.0/16"
   tags = {
     Name = "custom"
+  }
+}
+
+// Creating subnet 1
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.custom.id
+  cidr_block = ["57.95.0.1/24"]
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+// Creating subnet 2
+resource "aws_subnet" "subnet2" {
+  vpc_id = aws_vpc.custom.id
+  cidr_block = ["57.95.1.1/24"]
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "Private Subnet"
   }
 }
