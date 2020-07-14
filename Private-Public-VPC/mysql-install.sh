59354d090fdb603194d2731e167f258e9656e9e0
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-07-14 15:33:40 +0530

    Updated with Mysql Internals!

--- Private-Public-VPC/mysql-install.sh
+++ Private-Public-VPC/mysql-install.sh
@@ -2,4 +2,8 @@
 "sudo yum update -y",
 "sudo yum install mysql php-mysqlnd -y",
 "sudo systemctl start mysqld",
-"sudo systemctl enable mysqld"
+"sudo systemctl enable mysqld",
+"sudo mysql",
+"create database wordpress",
+"grant all on wordpress.* to wordpressUser@'localhost' identified by 'wordpressPassword';",
+"flush privileges;"
