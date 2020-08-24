8d7ce5d6e404287ecc1aaa18246e4cffb78f68aa
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-24 15:14:53 +0530

    Fully Working Kubernetes Deployment of WordPress on Minikube

--- MiniKube-AWS-RDS/main.tf
+++ MiniKube-AWS-RDS/main.tf
@@ -32,13 +32,33 @@ resource "kubernetes_deployment" "Webserver-Deployment" {
       spec {
         // Deploying a WordPress container!
         container {
-          name = "Webserver"
+          name = "webserver"
           image = var.image
           port {
             container_port = 80
           }
         }
       }
+    }
+  }
+}
+
+// Creating a Kubernetes Service type NodePort for the local Cluster OR Exposing the created Deployment
+resource "kubernetes_service" "WordPress" {
+  metadata {
+    name = "wordpress-service"
+  }
+  spec {
+    type = "NodePort"
+    selector = {
+      // Selector by which POD Deployment has to be selected
+      type = "webserver"
+    }
+    port {
+      port = 80
+      target_port = 80
+      protocol = "TCP"
+      name = "http"
     }
   }
 }
