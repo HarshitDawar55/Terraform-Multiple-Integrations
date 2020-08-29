13debdd08233aff72572c99ea7a1d2fd68c9f7da
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-29 19:27:21 +0530

    Started adding eks fargate profile

--- AWS(EKS)+Openstack(Magnum)/main.tf
+++ AWS(EKS)+Openstack(Magnum)/main.tf
@@ -19,10 +19,20 @@ provider "openstack" {
   region = "<Region Name>"
 }
 
-resource "aws_eks_cluster" "Production" {
-  name = ""
-  role_arn = ""
-  vpc_config {
-    subnet_ids = []
+resource "aws_eks_fargate_profile" "Production" {
+  # This resource will only be created when production workspace is selected. It is possible because of the below statement.
+  count = var.production ? 1 : 0
+
+  # Put your cluster Name
+  cluster_name = "Cassandra-Deployment"
+
+  # Put the Profile name of Fargate
+  fargate_profile_name = "Cassandra"
+  pod_execution_role_arn = "<AWS Resource Name of the IAM Role which grants permission for the EKS Fargate Profile>"
+
+  # Subnet IDS which are to attached to the AWS EKS Fargate Profile
+  subnet_ids = [""]
+  selector {
+    namespace = ["kube-system", "default"]
   }
 }
